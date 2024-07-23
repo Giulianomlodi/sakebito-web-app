f6f3718d59721d874bba646944567bcaa9d58d48
Giulianomlodi <giulianomlodi@gmail.com>
2024-07-23 16:59:34 +0200

    added connect wallet from mobile devices

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,7 +3,7 @@ import { polygon, sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "SAKEbito Web App",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "13e4f8639f1394481fc735b67e0133b6",
   chains: [
     polygon,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
