020479d3217b511e82f81bdc4ad0229a0052181c
Giulianomlodi <giulianomlodi@gmail.com>
2024-09-19 11:22:34 +0200

    added video on homepage

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -8,21 +8,20 @@ const Hero = () => {
         <section className={styles.heroSection}>
             <div className={styles.heroContainer}>
                 <div className={styles.imageColumn}>
-                    <Image
-                        src="/sakeIMG.jpg"
-                        alt="Hero Image"
-                        layout="fill"
-                        objectFit="cover"
-                        className={styles.heroImage}
+                    <video
+                        src="/video/NFT.mp4"
+                        className={styles.heroVideo}
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
                     />
                 </div>
                 <div className={styles.contentColumn}>
                     <Image className={styles.imageSake} src="/sakeIMG.jpg" alt="Sake Logo" width={400} height={400} />
                     <div className={styles.introSake}>
                         <Image src="/SAKEbito_name_black.png" alt="SAKEbito Logo" width={180} height={39} />
-                        <p>This is the Edition 1 Membership “ 一級 “, with only <strong>100 seats</strong> available.
-
-                        </p>
+                        <p>This is the Edition 1 Membership " 一級 ", with only <strong>100 seats</strong> available.</p>
                         <p> <strong>Join us</strong> now for an awesome <strong>Sake vibe</strong> and let's dive deeper into the world of <strong>premium</strong> Sake together!</p>
                     </div>
                     <div className={styles.mintOptions}>
